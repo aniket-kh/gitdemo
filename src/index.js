bd7da6da87df53b725cb7e4b5aff3f80ef165d3b
aniket-kh <aniketkhawale333@gmail.com>
2022-08-13 21:54:21 +0530

    xyz

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 var bodyParser = require('body-parser');
+const { default: mongoose } = require('mongoose')
 
 const route = require('./routes/route.js');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://Aniket:EpboWtWnytBBkVgl@cluster0.bxt9xv1.mongodb.net/Aniket-DB",{
+    useNewUrlParser : true
+
+})
+
+.then( () => console.log("MongoDb is Connected"))
+.catch( err => console.log(err))
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,3 +1,4 @@
+const { application } = require('express');
 const express = require('express');
 const abc = require('../introduction/intro')
 const {welcome} = require('../logger/logger');
@@ -18,5 +19,120 @@ router.get('/test-me', function (req, res) {
 
     res.send('Hello, World!')
 });
+// test-post-4
+
+
+
+
+    
+router.post("/test-post-4", function(req, res) {
+    let arr= [ 12, "functionup"]
+    let ele= req.body.element
+    arr.push(ele)
+    res.send(  { msg: arr , status: true }  )
+})
+  // problem 1
+
+let players =
+   [
+
+    {
+        "name": "gopal",
+        "dob": "1/09/1995",
+        "gender": "male",
+        "city": "delhi",
+        "sports": [
+            "soccer"
+        ]
+    },
+
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+        "name": "lokesh",
+        "dob": "1/1/1990",
+        "gender": "male",
+        "city": "mumbai",
+        "sports": [
+            "soccer"
+        ]
+    },]
+
+router.post("/players", function(req,res) {
+    let newplayer = req.body
+    let newplayersName = newplayer.name
+    let isNameRepeated = false
+
+    
+
+    for(let i = 0; i< players.length; i++) {
+        if(players[i].name == newplayersName){
+            isNameRepeated = true;
+             break;
+
+
+        }
+    }
+    if(isNameRepeated){
+        // it means playres ie exists
+        res.send("this player was already added!")
+    } else {
+        // new player entry
+        players.push(newplayer)
+        res.send(players)
+    }
+});
 module.exports = router;
 // adding this comment for no reason
+
+
+// problem 3
+
+let person = [
+    {
+        name:"pk",
+        age: 10,
+        votingstatus: false,
+    },
+    {
+        name:"sk",
+        age:20,
+        votingstatus: false,
+    },
+    {
+        name:"AA",
+        age:70,
+        votingstatus: false,
+    },
+    {
+        name:"sc",
+        age:5,
+        votingstatus: false,
+    },
+    {
+        name:"HO",
+        age: 40,
+        votingstatus: false,
+
+    }
+]
+
+router.post("/eligibleVoter/:age", function(req,res){
+    let eligibleAge = req.params.age;
+    let eligible =[];
+    for(let i=0; i<person.length; i++){
+        if(person[i].age>eligibleAge){
+            person[i].votingstatus = true;
+            eligible.push(person[i])
+        }
+    }
+    res.send(eligible);
+});
+
