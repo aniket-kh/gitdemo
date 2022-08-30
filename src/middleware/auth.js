0602ddbec27ae4478bf3715c33ab616979f3e417
aniket-kh <aniketkhawale333@gmail.com>
2022-08-30 23:11:16 +0530

    update

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,31 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
+const jwt=require('jsonwebtoken')
 
-    next()
+
+const authentication=async function(req,res,next){
+  try
+  {
+    let tokan = req.headers['x-auth-token'];
+  }
+  catch(arr){
+    if(!tokan) return res.status(402).send("Tokan is Required")
+    console.log(tokan)
+}
+try
+{
+    let decodedToken = jwt.verify(tokan, "functionup-plutonium-very-very-secret-key");
+}
+catch(arr){
+  if (!decodedToken)
+    return res.status(400).send({ status: false, msg: "token is invalid" });
 }
 
 
-const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
+    let paramId=req.params.userId
+    let tokenId=decodedToken.userId
+    if(paramId!=tokenId)
+       return res.send({status:false , msg:"Authorization failed ' User logged is not allowed to the requested users data"})
     next()
+
 }
+
+module.exports.authentication=authentication
--- src/routes/route.js
+++ src/routes/route.js
@@ -12,9 +12,9 @@ router.post("/login", userController.loginUser)
 
 //The userId is sent by front end
 router.get("/users/:userId", userController.getUserData)
-router.post("/users/:userId/posts", userController.postMessage)
+router.post("/users/:userId", userController.postMessage)
 
 router.put("/users/:userId", userController.updateUser)
-router.delete('/users/:userId', userController.deleteUser)
+router.delete('/users/:userId', userController.deleteuser)
 
 module.exports = router;
